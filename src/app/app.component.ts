657eac6141c68e32651aeda9ddb2f7e7a23a6bd8
hamma <mohamedhamma445@gmail.com>
2021-04-13 22:33:18 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'suiteFormation';
+export class AppComponent implements OnInit{
+  public form:FormGroup;
+  ngOnInit():void {
+    this.form = new FormGroup({
+      nom:new FormControl(''),
+      email:new FormControl(''),
+      passwoed:new FormControl('')
+    });
+  }
+  submit () : void {
+  console.log(this.form);
+  }
 }
