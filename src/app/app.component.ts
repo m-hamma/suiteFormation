1b810444f433f4ce527437f1ff0dda533d0d4753
hamma <mohamedhamma445@gmail.com>
2021-04-16 00:00:02 +0200

    VALIDATORS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,23 +10,41 @@ export class AppComponent implements OnInit{
   public form:FormGroup;
   ngOnInit():void {
     this.form = new FormGroup({
-      nom1:new FormControl(''),
-      email:new FormControl(''),
+      nom:new FormControl('',[this.validatorPaul, Validators.required],this.validatorAsynch),
+      email:new FormControl('',Validators.email),
       password:new FormControl('')
     });
-    this.form.addControl('nom', new FormControl(''));
+    this.form.addControl('nom1', new FormControl('',this.validatorPaul));
    this.form.removeControl('nom1');
    /* this.form.patchValue({
       email:'mouldha@gmail.com',
     });*/
+
     this.form.setValue({
           nom:'Hamma',
           email:'mould@gmail.com',
           password:'xxxx',
      });
   }
-  submit () : void {
-  this.form.reset();
-  console.log(this.form);
-  }
+   validatorPaul(formControl:FormControl):{[s:string]:boolean} {
+             if (formControl.value === 'paul') {
+               return {notPaul:true};
+             } else {
+               return null;
+             }
+   }
+   validatorAsynch(formControl:FormControl):Promise<{[s:string]:boolean} | null>{
+          return new Promise((resolve, reject) =>
+          {
+            setTimeout(()=> {
+              resolve(null);
+            },5000);
+          }
+          )
+
+      }
+      submit () : void {
+        //this.form.reset();
+        console.log(this.form.valid);
+      }
 }
