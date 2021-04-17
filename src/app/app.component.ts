a1a6504010e48fa1a046c05dcaf29bc1c44fa337
hamma <mohamedhamma445@gmail.com>
2021-04-18 00:50:38 +0200

    ajout gestion erreurs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
@@ -8,32 +8,46 @@ import {FormGroup, FormControl, Validators} from '@angular/forms';
 })
 export class AppComponent implements OnInit{
   public form:FormGroup;
+  @ViewChild('confirmation', {static:true})
+  public el:ElementRef<any>;
+
+  get nom() {
+      return this.form.get('nom');
+  }
+  get password() {
+      return this.form.get('password');
+  }
+  get confirmPassword() {
+   return this.el.nativeElement;
+  }
   ngOnInit():void {
     this.form = new FormGroup({
-      nom:new FormControl('',[this.validatorPaul, Validators.required],this.validatorAsynch),
+      nom:new FormControl('',[this.validatorPaul, Validators.required],/*this.validatorAsynch*/),
       email:new FormControl('',Validators.email),
-      password:new FormControl('')
+      password:new FormControl('', this.passwordMatch.bind(this))
     });
-    this.form.addControl('nom1', new FormControl('',this.validatorPaul));
-   this.form.removeControl('nom1');
+    //this.form.addControl('nom1', new FormControl('',this.validatorPaul));
+   //this.form.removeControl('nom1');
    /* this.form.patchValue({
       email:'mouldha@gmail.com',
     });*/
 
     this.form.setValue({
-          nom:'Hamma',
+          nom:'',
           email:'mould@gmail.com',
           password:'xxxx',
      });
+
+     //this.form.get('nom').
   }
-   validatorPaul(formControl:FormControl):{[s:string]:boolean} {
+   validatorPaul(formControl:FormControl):{[s:string]:boolean} | null{
              if (formControl.value === 'paul') {
                return {notPaul:true};
              } else {
                return null;
              }
    }
-   validatorAsynch(formControl:FormControl):Promise<{[s:string]:boolean} | null>{
+   /*validatorAsynch(formControl:FormControl):Promise<{[s:string]:boolean} | null{
           return new Promise((resolve, reject) =>
           {
             setTimeout(()=> {
@@ -42,9 +56,16 @@ export class AppComponent implements OnInit{
           }
           )
 
-      }
+      }*/
       submit () : void {
         //this.form.reset();
         console.log(this.form.valid);
+      }
+      passwordMatch (formControl:FormControl):{[s:string]:boolean} | null {
+        if (formControl.value !== this.el.nativeElement.value) {
+          return {notMatching:true};
+        } else {
+          return null;
+        }
       }
 }
