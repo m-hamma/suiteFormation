fd23baee9a28c100b92759b88718778e84abd1d2
hamma <mohamedhamma445@gmail.com>
2021-04-27 23:27:10 +0200

    suite formarray

--- src/app/form-array/form-array.component.ts
+++ src/app/form-array/form-array.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormArray, FormGroup, FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-form-array',
   templateUrl: './form-array.component.html',
@@ -11,6 +11,7 @@ export class FormArrayComponent implements OnInit {
     ngOnInit():void {
       this.form = new FormGroup({
         email:new FormControl(''),
+        hobbies : new FormArray([new FormControl('')]),
         password:new FormControl(''),
         nom:new FormControl(''),
       });
@@ -22,4 +23,13 @@ export class FormArrayComponent implements OnInit {
           //this.form.reset();
           console.log(this.form.valid);
         }
+        get hobbies () {
+          return this.form.get('hobbies') as FormArray;
+        }
+        addHobby () {
+          this.hobbies.push(new FormControl(''));
+        }
+        valider () {
+                 console.log(this.form);
+                }
 }
